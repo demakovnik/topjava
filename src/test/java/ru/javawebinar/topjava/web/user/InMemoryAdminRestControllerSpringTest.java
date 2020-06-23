e7b6b4f90b2a849bf5525a17e57234e25e05ed71
demakovnik <Nortumbria1986_>
2020-06-23 12:04:15 +0700

    Made the required corrections

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -7,16 +7,13 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
-import ru.javawebinar.topjava.service.UserService;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import static ru.javawebinar.topjava.UserTestData.NOT_FOUND;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
-@ContextConfiguration(classes = {AdminRestController.class, InMemoryUserRepository.class, UserService.class},
-        loader = AnnotationConfigContextLoader.class)
+@ContextConfiguration(locations = {"classpath:spring/spring-app.xml","classpath:spring-inmemory.xml"})
 
 @RunWith(SpringRunner.class)
 public class InMemoryAdminRestControllerSpringTest {
